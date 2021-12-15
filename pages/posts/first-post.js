6fdda4eb5dd2bf1e01e16cfb4f4848191aa552d4
Yuichiro Iijima <jimaichiro@gmail.com>
2021-12-16 02:03:09 +0900

    Image, Head, Styleの学習

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,32 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
+      <Image
+        src="/images/profile.jpeg"
+        height={144}
+        width={144}
+        alt="your name"
+      />
+
+    <style jsx>{`
+      h1 {
+        color: red;
+      }
+    `}
+    </style>
     </>
   )
 }
